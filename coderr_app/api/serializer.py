933dbd73628f707bcea52f603e5664d68dcd059b
Elias Lager <elias.lager01@gmail.com>
2026-06-01 17:39:39 +0200

    fix

--- coderr_app/api/serializer.py
+++ coderr_app/api/serializer.py
@@ -123,17 +123,6 @@ class OfferPatchSerializer(serializers.ModelSerializer):
         instance.description = validated_data.get('description', instance.description)
         instance.save()
 
-        # for detail_data in details_data:
-        #     detail_id = detail_data.get('id')
-        #     if detail_id:
-        #         detail = OfferDetail.objects.get(id=detail_id, offer=instance)
-        #         for attr, value, in detail_data.items():
-        #             setattr(detail, attr, value)
-        #         detail.save()
-        #     else:
-        #         OfferDetail.objects.create(offer=instance, **detail_data)
-        # return instance
-
         for detail_data in details_data:
             offer_type = detail_data.get('offer_type')
             if not offer_type:
