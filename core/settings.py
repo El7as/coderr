16c687d75f0112d96feea84065704d6d7f6edb56
El7as <el7as.453@gmail.com>
2026-09-19 13:34:43 +0200

    fix

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5500",
     "http://127.0.0.1:5500",
-    "https://coderr-frontend.onrender.com",
+    "https://coderrfrontend.onrender.com",
 ]
 
 
